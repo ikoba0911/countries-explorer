c27c6d17c671a5d15c2417361b97701019352973
ikoba0911 <ikki.5884.dev@gmail.com>
2025-10-25 13:36:49 +0900

    refactor: refactor_error_mapping

--- CountriesExplorer/CountriesExplorerApp.swift
+++ CountriesExplorer/CountriesExplorerApp.swift
@@ -3,14 +3,19 @@ import ComposableArchitecture
 
 @main
 struct CountriesExplorerApp: App {
+    init() {
+        // Install HttpClient console error handler at app launch
+        HttpClientErrorHandling.installConsoleHandler()
+    }
+
     var body: some Scene {
         WindowGroup {
             CountryListView(
                 store: Store(initialState: CountryListFeature.State()) {
                     CountryListFeature()
-                        ._printChanges() // デバッグ用。不要なら削除
                 }
             )
         }
     }
 }
+
